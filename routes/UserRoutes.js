5e104b0229f16c20107727b4bf006a409258d5ca
amirnarvi <amirnarvi@gmail.com>
2024-06-23 12:41:33 +0530

    routes update files added

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { register, login } from "../controllers/UserController.js";
+import { register, login, updateUser, deleteUser, getUsers } from "../controllers/UserController.js";
 import { authMiddleware } from "../middleware/auth.js";
 
 router.post("/register", register);
 router.post("/login", login);
+router.put("/update/:id", authMiddleware, updateUser);
+router.delete("/delete/:id", deleteUser);
+router.get("/", getUsers)
 
 export default router;
